f9552d79d0273edf80eea4ea9fb74b2e44a11d2a
dennis <wellsdennis@protonmail.com>
2019-12-09 18:22:53 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,13 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-
+    # gets rid of the pylint error.
+    objects = models.Manager()
     def publish(self):
         self.published_date = timezone.now()
         self.save()
